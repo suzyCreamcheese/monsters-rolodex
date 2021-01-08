6df4098ae8965f0b84911e193a4eb0babfd3b214
frank.vanderstelt <a001855@c21523.ad.smhi.se>
2021-01-08 12:57:27 +0100

    Updates

---
+++ precache-manifest.5bcab6898e52cdfbc44b6123d54a7dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd46e560997746b99fb4c2a7e4d599c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e149d7a0b2a0577e0195",
+    "url": "/monsters-rolodex/static/css/main.006a11f6.chunk.css"
+  },
+  {
+    "revision": "e401ae67c6b7093e7a34",
+    "url": "/monsters-rolodex/static/js/2.b1d99e9a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.b1d99e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e149d7a0b2a0577e0195",
+    "url": "/monsters-rolodex/static/js/main.4c1c6eeb.chunk.js"
+  },
+  {
+    "revision": "ad3bc63c9e1cfece0dcf",
+    "url": "/monsters-rolodex/static/js/runtime-main.adaee98e.js"
+  }
+]);
